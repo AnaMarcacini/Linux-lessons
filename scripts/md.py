420b3f40a5792feba3e91de4b1abe04b696ad5b6
AnaMarcacini <ahmarcacini@gmail.com>
2024-12-27 05:44:03 -0300

    erro script

--- scripts/md.py
+++ scripts/md.py
@@ -64,7 +64,7 @@ def escrever_indice(arquivo_md,indice,conteudo):
 
         for nivel, titulo, link in indice:
             indentacao = "&nbsp;&nbsp;&nbsp;&nbsp;" * (nivel - 1)
-            file.write(f'{indentacao} - <a href="link">{titulo}</a><br>')  # Adiciona cada título formatado
+            file.write(f'{indentacao} - <a href="#{link}">{titulo}</a><br>')  # Adiciona cada título formatado
             file.write("\n")
         file.writelines(conteudo)
 
